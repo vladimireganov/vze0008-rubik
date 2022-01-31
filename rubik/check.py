fa6a336c7daceea37359658e895dc5c11f69a554
Vladimir Eganov <38359772+vladimireganov@users.noreply.github.com>
2022-01-31 17:45:43 -0600

    done

--- rubik/check.py
+++ rubik/check.py
@@ -2,9 +2,58 @@ import rubik.cube as rubik
 
 def _check(parms):
     result={}
+    key = {}
     encodedCube = parms.get('cube',None)       
     if(encodedCube == None):
         result['status'] = 'error: xxx'
-    else:
-        result['status'] = 'ok'
+        return result
+    elif (not(isinstance(encodedCube, str))):
+        result['status'] = 'error: not str'
+        return result 
+    elif ( len (encodedCube)!= 54):
+        result['status'] = 'error: len'
+        return result
+    for i in range(len(encodedCube)):
+        if encodedCube[i] in key:
+            key[encodedCube[i]] += 1
+        else: 
+            key[encodedCube[i]] = 1
+    if len(key) != 6:
+        result['status'] = 'error: not 6 unique'
+        return result
+    for i in key:
+        if key[i] != 9:
+            result['status'] = 'error: not 9 occurences'
+            return result
+    middle_face = []
+    middle_face.append(encodedCube[4])
+    middle_face.append(encodedCube[14])
+    middle_face.append(encodedCube[23])
+    middle_face.append(encodedCube[32])
+    middle_face.append(encodedCube[41])
+    middle_face.append(encodedCube[50])
+    
+    for i in range(5):
+        if middle_face[i+1] == middle_face[0]:
+            result['status'] = 'error: middle face diff color'
+            return result
+    for i in range(4):
+        if middle_face[i+2] == middle_face[1]:
+            result['status'] = 'error: middle face diff color'
+            return result
+    for i in range(3):
+        if middle_face[i+3] == middle_face[2]:
+            result['status'] = 'error: middle face diff color'
+            return result
+    if middle_face[3] == middle_face[4]:
+            result['status'] = 'error: middle face diff color'
+            return result
+    elif middle_face[3] == middle_face[5]:
+            result['status'] = 'error: middle face diff color'
+            return result
+    elif middle_face[4] == middle_face[5]:
+            result['status'] = 'error: middle face diff color'
+            return result
+    
+    result['status'] = 'ok'
     return result
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -10,4 +10,46 @@ class CheckTest(TestCase):
         self.assertIn('status', result)
         status = result.get('status', None)
         self.assertEqual(status, 'ok')
-
+        
+    # sad path 
+    def test_check_length(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww12'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: len')
+        
+    def test_check_str(self):
+        parm = {'op':'check',
+                'cube':123}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: not str')
+    
+    def test_check_length_6_unique(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwtreqk'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: not 6 unique')
+        
+    
+    def test_check_9_occurences(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggooowoooooyyyyyyyyyyyyyyyyyy'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: not 9 occurences')
+    
+    
+    def test_check_middle_face_unique_color(self):
+        parm = {'op':'check',
+                'cube':'bbbbybbbbrrrryrrrrgggggggggooooooooobwryyyyyywwwwywwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: middle face diff color')
